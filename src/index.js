b3998effddf2a31b8e865ebfd0c006a8bc1d5f91
Dzmitry Yatsyna <dyfromby@gmail.com>
2020-03-12 01:15:04 +0300

    feat: add decode function

--- src/index.js
+++ src/index.js
@@ -37,9 +37,31 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const space = "**********";
+
 function decode(expr) {
-    // write your solution here
-}
+  let result = "";
+  let sustring = "";
+  let letter = "";
+  for (let i=0; i < expr.length; i+=10) {
+    substring = expr.slice(i, i+10).trim("0");
+    if (substring == space) {
+      result+=" ";
+      continue;
+    };
+    for (let j=0; j<substring.length; j+2) {
+      let part = substring.slice(j, j+2);
+      if (part == "10") {
+        letter += "."; 
+      }
+      else if (part == "11") {
+        letter += "-";
+      };
+    };
+    result+= MORSE_TABLE[latter];
+  };
+  return result;
+};
 
 module.exports = {
     decode
